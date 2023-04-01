4b84464cf9a7694627c2e84a3636da707188393a
aliogorobinna <aliogor7509@yahoo.com>
2023-04-01 09:51:00 +0200

    feat: s3 backend for statefile storage added

--- main.tf
+++ main.tf
@@ -9,3 +9,11 @@ Name = "Class30"
 Environment = "Prod"
   }
 }
+terraform {
+  backend "s3" {
+    bucket = "obinnatfstate-bucket"
+    key = "prod/tfstate.tfstate"
+    region = "us-east-2"
+
+  }
+}
